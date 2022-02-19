708254fb2b2e47facb96cf8fbc698b8532cfabc7
vladtymo <tymo.vlad@gmail.com>
2022-02-19 16:20:08 +0200

    Example of function overloading.

--- 02_array_in_arguments/Source.cpp
+++ 02_array_in_arguments/Source.cpp
@@ -12,14 +12,14 @@ int main()
 	int a = 10;
 	int b = 20;
 	cout << "a = " << a << ", b = " << b << endl;
-	mySwap(a, b); // при модифікації змінних у функції оригінали не змінюються
+	mySwap(a, b); // при модифікації змінних у функції - оригінали не змінюються
 	cout << "a = " << a << ", b = " << b << endl;
 
 	const int size = 10;
 	int arr[size] = { 3, 5, 1, 7, -4, 0, 9, 5, 6, 1 };
 
 	showArray(arr, size);
-	incrementArray(arr, size, 3); // при модифікації елементів масива у функції змінюється оригінал
+	incrementArray(arr, size, 3); // при модифікації елементів масива у функції - змінюється оригінал
 	showArray(arr, size);
 	incrementArray(arr, size); // value by default = 1
 	showArray(arr, size);
---
+++ 03_overloading/Source.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+// Function Overloading: функції з однаковою назвою, але різною сигнатурою
+// сигнатура - характеристика параметрів функції, їх типи та кількість
+
+int getRandom()
+{
+	return rand();
+}
+int getRandom(int max)
+{
+	return rand() % max;
+}
+int getRandom(int min, int max)
+{
+	return min + rand() % (max - min + 1);
+}
+
+int main()
+{
+	cout << "Random to 100: " << getRandom(100) << endl;
+	cout << "Random: " << getRandom() << endl;
+	cout << "Random from 50 to 70: " << getRandom(50, 70) << endl;
+
+	return 0;
+}
